7d344d34e3ef7aa35ab53cba8bfdaf2bfccafb51
Muhammad Rahman <muhammad.rahman@tkxel.io>
2022-08-10 15:34:10 +0500

    install tag manager.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-000000'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
